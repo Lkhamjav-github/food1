6efee2d142d4fe0527281d63471c7da284aafc2b
ReiGnZ <macbook@lkhamjav-macbook.local>
2024-10-09 18:51:15 +0800

    title bar icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,34 @@
+//Header.tsx
 import React from "react";
 
-export const Header = () => {
+type HeaderProps = {
+  scrollToHero: () => void;
+  scrollToProduct: () => void;
+  scrollToContact: () => void;
+  scrollToFooter: () => void;
+};
+export const Header: React.FC<HeaderProps> = ({
+  scrollToHero,
+  scrollToProduct,
+  scrollToContact,
+  scrollToFooter,
+}) => {
   return (
     <div className="flex justify-center items-center py-6">
       <div className="flex justify-between w-[80%]">
-        <a href="Home">Home</a>
-        <a href="Home">Our Products</a>
+        <a href="#" onClick={scrollToHero}>
+          Home
+        </a>
+        <a href="#" onClick={scrollToProduct}>
+          Our Products
+        </a>
         <img className="w-18 h-16" src="../logo.png" alt="" />
-        <a href="Home">Home</a>
-        <a href="Home">Contact us</a>
+        <a href="#" onClick={scrollToContact}>
+          Home
+        </a>
+        <a href="#" onClick={scrollToFooter}>
+          Contact us
+        </a>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,57 @@ import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { Products } from "@/components/Products";
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
+  const scrollToHero = () => {
+    const heroElement = document.getElementById("hero");
+    if (heroElement) {
+      heroElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const scrollToProduct = () => {
+    const productElement = document.getElementById("product");
+    if (productElement) {
+      productElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const scrollToContact = () => {
+    const contactElement = document.getElementById("contact");
+    if (contactElement) {
+      contactElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const scrollToFooter = () => {
+    const footerElement = document.getElementById("footer");
+    if (footerElement) {
+      footerElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <>
-      <Header />
+      <Head>
+        <title>GooLife Lores</title>
+        <meta name="description" content="Goolife" key="desc" />
+        <meta property="og:image" content="goolife.png" />
+        <link
+          className="rounded-full"
+          rel="icon"
+          type="/logo.png"
+          href="https://scontent.fuln6-2.fna.fbcdn.net/v/t39.30808-6/441059849_122162920268062624_593798740616597924_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=b_ALVjlB08oQ7kNvgHs6qYJ&_nc_ht=scontent.fuln6-2.fna&_nc_gid=Axw3NbtxYD8HQVC4gEvlJjh&oh=00_AYDrKzgsJQ4AJTUnqEc2DVpXaZOWV3hUiTsG-PNHvnO5xQ&oe=670C2011"
+        ></link>
+      </Head>
+      <Header
+        scrollToHero={scrollToHero}
+        scrollToProduct={scrollToProduct}
+        scrollToContact={scrollToContact}
+        scrollToFooter={scrollToFooter}
+      />
       <Hero />
       <Products />
       <Contact />
